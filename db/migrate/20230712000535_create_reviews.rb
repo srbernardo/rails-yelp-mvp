13bc422df0eb8d60e96965cb478fe9a117ad33cf
bernardo brotelli <b.brotelle@gmail.com>
2023-07-11 21:47:37 -0300

    Fixed the atributes rantig to rating

--- db/migrate/20230712000535_create_reviews.rb
+++ db/migrate/20230712000535_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
-      t.integer :rantig
+      t.integer :rating
       t.text :content
       t.references :restaurant, null: false, foreign_key: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,15 +13,15 @@
 ActiveRecord::Schema[7.0].define(version: 2023_07_12_000535) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
-    t.string "adsress"
-    t.integer "phone_number"
+    t.string "address"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer "rantig"
+    t.integer "rating"
     t.text "content"
     t.integer "restaurant_id", null: false
     t.datetime "created_at", null: false
