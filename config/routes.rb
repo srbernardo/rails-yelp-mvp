4c17f0a2e484e8c9e55fad93f0af9b5ac9ae9ff8
bernardo brotelli <b.brotelle@gmail.com>
2023-07-11 21:32:21 -0300

    Ad od routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  resources :restaurants
-
+  get 'reviews/new'
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
 end
