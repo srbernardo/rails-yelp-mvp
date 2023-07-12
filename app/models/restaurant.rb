585defbbed0ad2f3b6990319148cd985aeb6b455
bernardo brotelli <b.brotelle@gmail.com>
2023-07-11 21:28:33 -0300

    Add of validates

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant < ApplicationRecord
-  has_many: :reviews, dependent: :destroy
+  has_many :reviews, dependent: :destroy
+
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+
+  validates :category, presence: true, inclusion: { in: CATEGORY }
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :address, presence: true
+
 end
