50eb5374152f2f9f5890cbfecf8cc0314b3e3a18
bernardo brotelli <b.brotelle@gmail.com>
2023-09-13 22:52:13 -0300

    Capitalize categories

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,11 +1,10 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
-  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+  CATEGORY = ["Chinese", "Italian", "Japanese", "French", "Belgian", "Brazilian"]
 
   validates :category, presence: true, inclusion: { in: CATEGORY }
   validates :name, presence: true
   validates :category, presence: true
   validates :address, presence: true
-
 end
