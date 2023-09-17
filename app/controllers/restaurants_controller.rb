8edadb72d5729c6381a468aacfbdf67aea197631
bernardo brotelli <b.brotelle@gmail.com>
2023-09-17 17:03:44 -0300

    Redirect to root path when destroy

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -50,11 +50,7 @@ class RestaurantsController < ApplicationController
   # DELETE /restaurants/1 or /restaurants/1.json
   def destroy
     @restaurant.destroy
-
-    respond_to do |format|
-      format.html { redirect_to restaurants_url, notice: "Restaurant was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    redirect_to root_path, notice: "Restaurant was successfully destroyed."
   end
 
   private
